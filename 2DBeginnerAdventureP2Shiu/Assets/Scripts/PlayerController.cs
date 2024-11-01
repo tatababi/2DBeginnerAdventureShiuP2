34ead2e859c6a787b1e3af89ad76df8b0fd4bcee
tatababi <zyannotlion15@gmail.com>
2024-11-01 09:59:34 -0700

    damagezones and interactables
    added colledctable interactables and almost completed coding damage zones

--- 2DBeginnerAdventureP2Shiu/Assets/Scripts/PlayerController.cs
+++ 2DBeginnerAdventureP2Shiu/Assets/Scripts/PlayerController.cs
@@ -1,9 +1,21 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float speed = 3.0f;
+    public int maxHealth = 5;
+    public float timeInvincible = 2;
+
+    public int health { get { return currentHealth; } }
+    int currentHealth;
+
+    bool inInvincible;
+    float invincibleTimer;
+       
+
     Rigidbody2D rigidbody2d;
     float horizontal;
     float vertical;
@@ -11,6 +23,8 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
+        currentHealth = maxHealth;
+       
     }
 
     // Update is called once per frame
@@ -19,15 +33,23 @@ public class PlayerController : MonoBehaviour
         horizontal = Input.GetAxis("Horizontal");
         vertical = Input.GetAxis("Vertical");
 
+        
+
 
 
     }
     void FixedUpdate()
     {
         Vector2 position = rigidbody2d.position;
-        position.x = position.x + 3.0f * horizontal * Time.deltaTime;
-        position.y = position.y + 3.0f * vertical * Time.deltaTime; ;
+        position.x = position.x + speed * horizontal * Time.deltaTime;
+        position.y = position.y + speed * vertical * Time.deltaTime; ;
 
         rigidbody2d.MovePosition(position);
+    }
+    public void ChangeHealth(int amount)
+    {
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        Debug.Log(currentHealth + "/" + maxHealth);
+
     }
 }
