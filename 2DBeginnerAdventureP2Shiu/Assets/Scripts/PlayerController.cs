4e686397a29bbef74b4b26e557c2b5e6b1c8c7df
tatababi <728472@student.dusd.net>
2024-11-21 22:15:53 -0800

    hope
    I DID IT I FINISHED ANIMATIONS!! I figured out my error and fixed it and once I did that I was able to finish the ducko animation quickly

--- 2DBeginnerAdventureP2Shiu/Assets/Scripts/PlayerController.cs
+++ 2DBeginnerAdventureP2Shiu/Assets/Scripts/PlayerController.cs
@@ -7,7 +7,7 @@ public class PlayerController : MonoBehaviour
 {
     public float speed = 3.0f;
     public int maxHealth = 5;
-    public float timeInvincible = 2;
+    public float timeInvincible = 2.0f;
 
     public int health { get { return currentHealth; } }
     int currentHealth;
@@ -19,12 +19,18 @@ public class PlayerController : MonoBehaviour
     Rigidbody2D rigidbody2d;
     float horizontal;
     float vertical;
+
+    Animator animator;
+    Vector2 lookDirection = new Vector2(1, 0);
+    
+
+
     // Start is called before the first frame update
     void Start()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
         currentHealth = maxHealth;
-       
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -32,6 +38,18 @@ public class PlayerController : MonoBehaviour
     {
         horizontal = Input.GetAxis("Horizontal");
         vertical = Input.GetAxis("Vertical");
+        
+        Vector2 move = new Vector2(horizontal, vertical);
+
+        if(!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f))
+        {
+            lookDirection.Set(move.x, move.y);
+            lookDirection.Normalize();
+            animator.SetFloat("Look X", lookDirection.x);
+            animator.SetFloat("Look Y", lookDirection.y);
+            animator.SetFloat("Speed", move.magnitude);
+        }
+
 
         if(isInvincible)
         {
@@ -59,6 +77,7 @@ public class PlayerController : MonoBehaviour
     {
         if (amount < 0)
         {
+            animator.SetTrigger("Hit");
             if (isInvincible)
             {
                 return;
